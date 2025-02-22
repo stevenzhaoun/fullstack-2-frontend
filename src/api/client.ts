60127c0238f9df9ba45b5bf777b5eea410a2997c
Steven Zhao <stevenzhao0819@gmail.com>
2025-02-21 19:16:35 -0800

    Update client.ts

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const client = axios.create({
-    baseURL: import.meta.env.API_URL || 'http://localhost:8888',
+    // baseURL: import.meta.env.API_URL || 'http://localhost:8888',/
+    baseURL: 'https://fullstack-2-backend.onrender.com'
 })
 
 export default client
